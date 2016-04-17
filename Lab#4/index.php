6f06c79bf756c4db51422a9dcc5bc06efdf48be3
Tudor Tanasescu <ttanasescu@gmail.com>
2016-04-17 18:37:29 +0300

    Index page created

---
+++ Lab#4/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<link rel="stylesheet" type="text/css" href="/styles/top_style.css">
+	</head>
+
+	<body bgcolor="#EEEEEE">
+		<div class="Page">
+			<div>
+				<div class="TopLeft" ></div>
+				<div class="TopCenter" ></div>
+				<div class="TopRight" ></div>
+			</div>
+			<div class="Block">
+				<div class="Title">Arhivă Meteo</div>
+				<div class="TopBody">
+					<p>Alege data, începînd cu care se vor afişa prognozele meteo pentru 3 zile, în Chişinău.</p>
+					<form>
+						<input type="date" name="date"  value="2016-04-16" min="2016-04-16" max="2016-04-23" />
+					</form>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
