b1787d2f07efc0939d9d1f9fb63727fed07d07e6
Tudor Tanasescu <ttanasescu@gmail.com>
2016-05-19 17:48:31 +0300

    Lab#6's first commit

---
+++ Lab#6/Akari/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Akari
+{
+     public partial class Form1 : Form
+     {
+          int[,] puzzle_matrix;
+          int puzzle_columns;
+          int puzzle_rows;
+
+          int practice_count;
+          int easy_count;
+          int normal_count;
+
+          public Form1()
+          {
+               InitializeComponent();
+          }
+     }
+}
