0ad4a5343bf273d9bcd93a240dfdff957a4731b7
Tudor Tanasescu <ttanasescu@gmail.com>
2016-05-21 22:10:26 +0300

    Addedd function to light up the table

--- Lab#6/Akari/Core_Module.cs
+++ Lab#6/Akari/Core_Module.cs
@@ -101,5 +101,263 @@ namespace Akari
 
                normal_count = Int32.Parse(xmlNode.Attributes["value"].Value);
           }
+
+          void ClickOnButton(int I, int J)
+          {
+               Point UP = new Point(I - 1, J);
+               Point DOWN = new Point(I + 1, J);
+               Point LEFT = new Point(I, J - 1);
+               Point RIGHT = new Point(I, J + 1);
+               bool go_left = false, go_right = false, go_up = false, go_down = false;
+
+               int SET;
+
+               if (puzzle_matrix[I, J] / 10 == 7)
+               {
+                    puzzle_matrix[I, J] = 60 + puzzle_matrix[I, J] % 10;
+                    SET = 1;
+               }
+               else if (puzzle_matrix[I, J] / 10 == 6)
+               {
+                    puzzle_matrix[I, J] = 70 + puzzle_matrix[I, J] % 10;
+                    SET = -1;
+               }
+               else return;
+
+               UpdateButton(I, J);
+
+               for (int i = 0; i < puzzle_columns; i++)
+               {
+                    if (UP.X > -1)
+                    {
+                         if (puzzle_matrix[UP.X, UP.Y] / 10 == 7)
+                         {
+                              puzzle_matrix[UP.X, UP.Y] += SET;
+                              UpdateButton(UP.X--, UP.Y);
+                         }
+                         else
+                         {
+                              if (puzzle_matrix[UP.X, UP.Y] / 10 == 6)
+                                   go_down = true;
+                              if (puzzle_matrix[UP.X, UP.Y] / 10 < 6)
+                              {
+                                   if (i == 0) puzzle_matrix[UP.X, UP.Y] += SET;
+                              }
+                              else
+                              {
+                                   puzzle_matrix[UP.X, UP.Y] += SET;
+                              }
+                              UpdateButton(UP.X, UP.Y);
+                              UP.X = -1;
+                         }
+                    }
+
+                    if (DOWN.X < puzzle_rows)
+                    {
+                         if (puzzle_matrix[DOWN.X, DOWN.Y] / 10 == 7)
+                         {
+                              puzzle_matrix[DOWN.X, DOWN.Y] += SET;
+                              UpdateButton(DOWN.X++, DOWN.Y);
+                         }
+                         else
+                         {
+                              if (puzzle_matrix[DOWN.X, DOWN.Y] / 10 == 6)
+                                   go_up = true;
+                              if (puzzle_matrix[DOWN.X, DOWN.Y] / 10 < 6)
+                              {
+                                   if (i == 0) puzzle_matrix[DOWN.X, DOWN.Y] += SET;
+                              }
+                              else
+                              {
+                                   puzzle_matrix[DOWN.X, DOWN.Y] += SET;
+                              }
+                              UpdateButton(DOWN.X, DOWN.Y);
+                              DOWN.X = puzzle_rows;
+                         }
+                    }
+
+                    if (LEFT.Y > -1)
+                    {
+                         if (puzzle_matrix[LEFT.X, LEFT.Y] / 10 == 7)
+                         {
+                              puzzle_matrix[LEFT.X, LEFT.Y] += SET;
+                              UpdateButton(LEFT.X, LEFT.Y--);
+                         }
+                         else
+                         {
+                              if (puzzle_matrix[LEFT.X, LEFT.Y] / 10 == 6)
+                                   go_right = true;
+                              if (puzzle_matrix[LEFT.X, LEFT.Y] / 10 < 6)
+                              {
+                                   if (i == 0) puzzle_matrix[LEFT.X, LEFT.Y] += SET;
+                              }
+                              else
+                              {
+                                   puzzle_matrix[LEFT.X, LEFT.Y] += SET;
+                              }
+                              UpdateButton(LEFT.X, LEFT.Y);
+                              LEFT.Y = -1;
+                         }
+                    }
+
+                    if (RIGHT.Y < puzzle_columns)
+                    {
+                         if (puzzle_matrix[RIGHT.X, RIGHT.Y] / 10 == 7)
+                         {
+                              puzzle_matrix[RIGHT.X, RIGHT.Y] += SET;
+                              UpdateButton(RIGHT.X, RIGHT.Y++);
+                         }
+                         else
+                         {
+                              if (puzzle_matrix[RIGHT.X, RIGHT.Y] / 10 == 6)
+                                   go_left = true;
+                              if (puzzle_matrix[RIGHT.X, RIGHT.Y] / 10 < 6)
+                              {
+                                   if (i == 0) puzzle_matrix[RIGHT.X, RIGHT.Y] += SET;
+                              }
+                              else
+                              {
+                                   puzzle_matrix[RIGHT.X, RIGHT.Y] += SET;
+                              }
+                              UpdateButton(RIGHT.X, RIGHT.Y);
+                              RIGHT.Y = puzzle_columns;
+                         }
+                    }
+               }
+
+               SET = -SET;
+
+               if (go_up)
+               {
+                    UP = new Point(I - 1, J);
+                    int i = 0;
+
+                    while (UP.X > -1)
+                    {
+                         if (i == 0 && puzzle_matrix[UP.X, UP.Y] / 10 < 6)
+                         {
+                              break;
+                         }
+
+                         if (puzzle_matrix[UP.X, UP.Y] / 10 == 7)
+                         {
+                              puzzle_matrix[UP.X, UP.Y] += SET;
+                              UpdateButton(UP.X--, UP.Y);
+                         }
+                         else
+                         {
+                              if (puzzle_matrix[UP.X, UP.Y] / 10 < 6)
+                              {
+                                   if (i++ == 0) puzzle_matrix[UP.X, UP.Y] += SET;
+                              }
+                              else
+                              {
+                                   puzzle_matrix[UP.X, UP.Y] += SET;
+                              }
+                              UpdateButton(UP.X, UP.Y);
+                              UP.X = -1;
+                         }
+                    }
+               }
+
+               if (go_down)
+               {
+                    DOWN = new Point(I + 1, J);
+                    int i = 0;
+
+                    while (DOWN.X < puzzle_rows)
+                    {
+                         if (i == 0 && puzzle_matrix[DOWN.X, DOWN.Y] / 10 < 6)
+                         {
+                              break;
+                         }
+
+                         if (puzzle_matrix[DOWN.X, DOWN.Y] / 10 == 7)
+                         {
+                              puzzle_matrix[DOWN.X, DOWN.Y] += SET;
+                              UpdateButton(DOWN.X++, DOWN.Y);
+                         }
+                         else
+                         {
+                              if (puzzle_matrix[DOWN.X, DOWN.Y] / 10 < 6)
+                              {
+                                   if (i++ == 0) puzzle_matrix[DOWN.X, DOWN.Y] += SET;
+                              }
+                              else
+                              {
+                                   puzzle_matrix[DOWN.X, DOWN.Y] += SET;
+                              }
+                              UpdateButton(DOWN.X, DOWN.Y);
+                              DOWN.X = puzzle_rows;
+                         }
+                    }
+               }
+
+               if (go_left)
+               {
+                    LEFT = new Point(I, J - 1);
+                    int i = 0;
+
+                    while (LEFT.Y > -1)
+                    {
+                         if (i == 0 && puzzle_matrix[LEFT.X, LEFT.Y] / 10 < 6)
+                         {
+                              break;
+                         }
+
+                         if (puzzle_matrix[LEFT.X, LEFT.Y] / 10 == 7)
+                         {
+                              puzzle_matrix[LEFT.X, LEFT.Y] += SET;
+                              UpdateButton(LEFT.X, LEFT.Y--);
+                         }
+                         else
+                         {
+                              if (puzzle_matrix[LEFT.X, LEFT.Y] / 10 < 6)
+                              {
+                                   if (i++ == 0) puzzle_matrix[LEFT.X, LEFT.Y] += SET;
+                              }
+                              else
+                              {
+                                   puzzle_matrix[LEFT.X, LEFT.Y] += SET;
+                              }
+                              UpdateButton(LEFT.X, LEFT.Y);
+                              LEFT.Y = -1;
+                         }
+                    }
+               }
+
+               if (go_right)
+               {
+                    RIGHT = new Point(I, J + 1);
+                    int i = 0;
+
+                    while (RIGHT.Y < puzzle_columns)
+                    {
+                         if (i == 0 && puzzle_matrix[RIGHT.X, RIGHT.Y] / 10 < 6)
+                         {
+                              break;
+                         }
+
+                         if (puzzle_matrix[RIGHT.X, RIGHT.Y] / 10 == 7)
+                         {
+                              puzzle_matrix[RIGHT.X, RIGHT.Y] += SET;
+                              UpdateButton(RIGHT.X, RIGHT.Y++);
+                         }
+                         else
+                         {
+                              if (puzzle_matrix[RIGHT.X, RIGHT.Y] / 10 < 6)
+                              {
+                                   if (i++ == 0) puzzle_matrix[RIGHT.X, RIGHT.Y] += SET;
+                              }
+                              else
+                              {
+                                   puzzle_matrix[RIGHT.X, RIGHT.Y] += SET;
+                              }
+                              UpdateButton(RIGHT.X, RIGHT.Y);
+                              RIGHT.Y = puzzle_columns;
+                         }
+                    }
+               }
+          }
      }
 }
