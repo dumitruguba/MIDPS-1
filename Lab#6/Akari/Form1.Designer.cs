f2a9b9710588a6f2a637083963965d1909116544
dumitruguba <dumitru.guba@gmail.com>
2016-05-21 21:21:20 +0300

    Added GUI menu & puzzle table

--- Lab#6/Akari/Form1.Designer.cs
+++ Lab#6/Akari/Form1.Designer.cs
@@ -28,33 +28,109 @@
           /// </summary>
           private void InitializeComponent()
           {
-               this.button1 = new System.Windows.Forms.Button();
-               this.SuspendLayout();
-               // 
-               // button1
-               // 
-               this.button1.Location = new System.Drawing.Point(205, 95);
-               this.button1.Name = "button1";
-               this.button1.Size = new System.Drawing.Size(75, 23);
-               this.button1.TabIndex = 0;
-               this.button1.Text = "button1";
-               this.button1.UseVisualStyleBackColor = true;
-               // 
-               // Form1
-               // 
-               this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-               this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-               this.ClientSize = new System.Drawing.Size(292, 273);
-               this.Controls.Add(this.button1);
-               this.Name = "Form1";
-               this.Text = "Form1";
-               this.ResumeLayout(false);
+            this.menu_panel = new System.Windows.Forms.Panel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.problems_panel = new System.Windows.Forms.Panel();
+            this.stage_panel = new System.Windows.Forms.Panel();
+            this.menu_panel.SuspendLayout();
+            this.problems_panel.SuspendLayout();
+            this.stage_panel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menu_panel
+            // 
+            this.menu_panel.Controls.Add(this.button3);
+            this.menu_panel.Controls.Add(this.button2);
+            this.menu_panel.Controls.Add(this.button1);
+            this.menu_panel.Location = new System.Drawing.Point(97, 65);
+            this.menu_panel.Name = "menu_panel";
+            this.menu_panel.Size = new System.Drawing.Size(200, 250);
+            this.menu_panel.TabIndex = 0;
+            // 
+            // button3
+            // 
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LightGray;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(63, 157);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 30);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Normal";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.menu_button_Click);
+            // 
+            // button2
+            // 
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LightGray;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(63, 106);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 30);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Easy";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.menu_button_Click);
+            // 
+            // button1
+            // 
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LightGray;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(63, 55);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 30);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Practice";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.menu_button_Click);
+            // 
+            // problems_panel
+            // 
+            this.problems_panel.Controls.Add(this.menu_panel);
+            this.problems_panel.Location = new System.Drawing.Point(11, 11);
+            this.problems_panel.Name = "problems_panel";
+            this.problems_panel.Size = new System.Drawing.Size(401, 372);
+            this.problems_panel.TabIndex = 1;
+            // 
+            // stage_panel
+            // 
+            this.stage_panel.Controls.Add(this.problems_panel);
+            this.stage_panel.Location = new System.Drawing.Point(1, 1);
+            this.stage_panel.Name = "stage_panel";
+            this.stage_panel.Size = new System.Drawing.Size(424, 395);
+            this.stage_panel.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(425, 396);
+            this.Controls.Add(this.stage_panel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menu_panel.ResumeLayout(false);
+            this.problems_panel.ResumeLayout(false);
+            this.stage_panel.ResumeLayout(false);
+            this.ResumeLayout(false);
 
           }
 
           #endregion
 
+          private System.Windows.Forms.Panel menu_panel;
+          private System.Windows.Forms.Button button3;
+          private System.Windows.Forms.Button button2;
           private System.Windows.Forms.Button button1;
+          private System.Windows.Forms.Panel problems_panel;
+          private System.Windows.Forms.Panel stage_panel;
 
 
      }
--- Lab#6/Akari/GUI_Module.cs
+++ Lab#6/Akari/GUI_Module.cs
@@ -9,7 +9,142 @@ using System.Windows.Forms;
 
 namespace Akari
 {
+
+    public class MyButton : Button
+    {
+        public int _i, _j;
+
+        public MyButton(int i, int j)
+        {
+            _i = i;
+            _j = j;
+
+            this.BackColor = Color.Black;
+            this.Font = new Font("Tahoma", 10f);
+            this.Font = new Font(this.Font, FontStyle.Bold);
+            this.FlatStyle = FlatStyle.Flat;
+            this.FlatAppearance.MouseOverBackColor = Color.Black;
+            this.FlatAppearance.MouseDownBackColor = Color.Black;
+            this.FlatAppearance.BorderSize = 0;
+        }
+    }
+
      public partial class Form1 : Form
      {
+         private void menu_button_Click(object sender, EventArgs e)
+         {
+             Button button = (Button)sender;
+             menu_panel.Hide();
+             difficulty = button.Text;
+             LoadProblems();
+             problems_panel.Show();
+         }
+
+         String difficulty;
+         String problem_index;
+         
+         private void LoadProblems()
+         {
+             var btnArray = new Button[4, 5];
+
+             int contor = 0;
+
+             if (difficulty == "Practice")
+                 contor = practice_count;
+             else if (difficulty == "Easy")
+                 contor = easy_count;
+             else if (difficulty == "Normal")
+                 contor = normal_count;
+             if (contor == 0)
+                 return;
+
+             for (int i = 0; i < 4; i++)
+             {
+                 for (int j = 0; j < 5; j++)
+                 {
+                     btnArray[i, j] = new Button();
+                     btnArray[i, j].Size = new Size(30, 30);
+                     problems_panel.Controls.Add(btnArray[i, j]);
+                     btnArray[i, j].Click += new EventHandler(btnProblem_Click);
+                     btnArray[i, j].FlatAppearance.BorderSize = 0;
+                     btnArray[i, j].BackColor = Color.SteelBlue;
+                     btnArray[i, j].FlatStyle = FlatStyle.Flat;
+                     btnArray[i, j].FlatAppearance.MouseOverBackColor = Color.SteelBlue;
+                     btnArray[i, j].FlatAppearance.MouseDownBackColor = Color.SteelBlue;
+                     btnArray[i, j].Text = ((j+1) +(i*5)).ToString();
+                     btnArray[i, j].Location = new Point(62 + j * 60, 80 + i * 60);
+                     if (--contor == 0) return;
+                 }
+             }
+         }
+
+         private void btnProblem_Click(object sender, EventArgs e)
+         {
+             Button button = (Button)sender;
+             problems_panel.Hide();
+             problem_index = button.Text;
+             LoadPuzzle(difficulty, problem_index);
+             LoadStage();
+         }
+
+         MyButton[,] btnStage;
+
+         private void LoadStage()
+         {
+             stage_panel.Hide();
+
+             btnStage = new MyButton[puzzle_rows, puzzle_columns];
+             int digit, x = 0, y = 0;
+
+             if (difficulty == "Practice") { 
+                 x = 115; 
+                 y = 75; 
+             }
+             else if (difficulty == "Easy" || difficulty == "Normal") { 
+                 x = 52; 
+                 y = 32; 
+             }
+
+             Label label = new Label();
+
+             label.BackColor = Color.DimGray;
+             label.Location = new Point(x-4, y-4);
+             label.Size = new Size(8 + 31 * puzzle_rows, 8 + 31 * puzzle_columns);
+
+             stage_panel.Controls.Add(label);
+
+             for (int i = 0; i < puzzle_rows; i++)
+             {
+                 for (int j = 0; j < puzzle_columns; j++)
+                 {
+                     btnStage[i, j] = new MyButton(i,j);
+                     btnStage[i, j].Size = new Size(30, 30);
+                     label.Controls.Add(btnStage[i, j]);
+                     digit = Convert.ToInt32(puzzle_matrix[i, j].ToString().Substring(0, 1));
+                     if (puzzle_matrix[i, j] >= 0 && puzzle_matrix[i, j] <= 49)
+                     {
+                         if (puzzle_matrix[i, j] / 10 == puzzle_matrix[i, j] % 10)
+                            btnStage[i, j].ForeColor = Color.Orange;
+                         else
+                             btnStage[i, j].ForeColor = Color.White;
+
+                         btnStage[i, j].Text = digit.ToString();
+                         btnStage[i, j].BackColor = Color.SteelBlue;
+                         btnStage[i, j].FlatAppearance.MouseOverBackColor = Color.SteelBlue;
+                         btnStage[i, j].FlatAppearance.MouseDownBackColor = Color.SteelBlue;
+                     }
+
+                     if (puzzle_matrix[i, j] >= 50 && puzzle_matrix[i, j] <= 59)
+                     {
+                         btnStage[i, j].Text = " ";
+                         btnStage[i, j].BackColor = Color.SteelBlue;
+                         btnStage[i, j].Enabled = false;
+                     }
+
+                     btnStage[i, j].Location = new Point(4 + j * 31, 4 + i * 31);
+                 }
+             }
+             stage_panel.Show();
+         }
      }
 }
